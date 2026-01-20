abdc8d916f2b197a80f57ff17ac004c78d602971
RishabhShenoy03 <rishabh2003@live.com>
2026-01-21 00:18:05 +0800

    Added JongBot file, deleted Duke.java

---
+++ src/main/java/JongBot.java
@@ -0,0 +1,8 @@
+public class JongBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Jongbot");
+        System.out.println("How may I help you?");
+        System.out.println();
+        System.out.println("Bye bye! See you soon!");
+    }
+}
