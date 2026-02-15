902ee7764c5965bed56e43441e33646b2ed27bac
RishabhShenoy03 <rishabhshenoy2003@gmail.com>
2026-02-16 01:37:58 +0800

    Changes for tonight

--- src/main/java/jongbot/Todo.java
+++ src/main/java/jongbot/Todo.java
@@ -10,4 +10,10 @@ public class Todo extends Task {
     public String toString() {
         return "[T]" + super.toString();
     }
+
+    @Override
+    public String toDataString() {
+        return "T | " + (isDone ? "1" : "0") + " | " + description;
+    }
+
 }
